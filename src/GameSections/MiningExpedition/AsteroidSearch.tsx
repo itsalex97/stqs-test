e98e34239d0b400a78678d5896a6401eb9ae7dc3
itsalex97 <alexmatthewtaylor@gmail.com>
2024-09-25 00:28:59 +0100

    Asteroid Search added

---
+++ src/GameSections/MiningExpedition/AsteroidSearch.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+function AsteroidSearch({apiToken, sysSymbol}) {
+    const [resp, setResp] = useState("");
+    const [form, setForm] = useState({ token: apiToken, systemSymbol: sysSymbol});
+
+    return(<>
+      <h3>Asteroid Search</h3>
+      <p>Have a mining drone? You're in luck!<br />You can use this drone to go excavate valuable ores and minerals from asteroids. Enter your Token and System Symbol to begin your search.</p>
+        <input name="api-token" placeholder="API Token" value={form.token} onChange={(e) => setForm({ ...form, token: e.currentTarget.value })} />
+        <input name="system-symbol" placeholder="System Symbol" value={form.systemSymbol} onChange={(e) => setForm({ ...form, systemSymbol: e.currentTarget.value })} />
+
+        <input type="submit" onClick={async () => {
+          const resp = await fetch("https://api.spacetraders.io/v2/systems/" + form.systemSymbol + "/waypoints?type=ENGINEERED_ASTEROID", {
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + form.token,
+            }
+          });
+    
+          const json = await resp.json();
+    
+          setResp(JSON.stringify(json, null, 2))
+        }} />
+        <pre>Response: {resp}</pre>
+      </>)
+}
+export default AsteroidSearch
